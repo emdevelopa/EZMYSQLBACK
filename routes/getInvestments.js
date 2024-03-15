dc6426ea637fb459fdf98d70031d9501af62453b
emdevelopa <gyimahsunday@gmail.com>
2024-03-15 07:46:20 +0100

    cool

--- routes/getInvestments.js
+++ routes/getInvestments.js
@@ -1,15 +1,44 @@
+// const { pool } = require("../db/database");
+// const db = require("../db/getCurrrentDB");
+
+// const investments = async (req, res) => {
+//   const userId = req.params.userId;
+//   if (userId) {
+//     const [getInvestments] = await pool.query(
+//       `SELECT * FROM ${db}.investments WHERE wallet_id = ? AND status='active'`,
+//       [userId]
+//     );
+//     // console.log();
+//     res.json({ investments: getInvestments[0] });
+//   }
+// };
+
+// module.exports = investments;
+
+
 const { pool } = require("../db/database");
 const db = require("../db/getCurrrentDB");
 
 const investments = async (req, res) => {
   const userId = req.params.userId;
   if (userId) {
-    const [getInvestments] = await pool.query(
-      `SELECT * FROM ${db}.investments WHERE wallet_id = ? AND status='active'`,
-      [userId]
-    );
-    // console.log();
-    res.json({ investments: getInvestments[0] });
+    try {
+      const tableExistsQuery = `SELECT COUNT(*) AS count FROM information_schema.tables WHERE table_schema = ? AND table_name = 'investments'`;
+      const [tableExistsResult] = await pool.query(tableExistsQuery, [db]);
+
+      if (tableExistsResult[0].count === 1) {
+        const investmentsQuery = `SELECT * FROM ${db}.investments WHERE wallet_id = ? AND status='active'`;
+        const [getInvestments] = await pool.query(investmentsQuery, [userId]);
+        res.json({ investments: getInvestments });
+      } else {
+        res.status(404).json({ error: "Investments table does not exist" });
+      }
+    } catch (error) {
+      console.error("Error fetching investments:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  } else {
+    res.status(400).json({ error: "User ID is missing" });
   }
 };
 
